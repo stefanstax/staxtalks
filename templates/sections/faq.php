4ec0f47daba21785876025958fb801c521922114
Stefan Stax <stefanstaxbusiness@gmail.com>
2022-10-23 14:38:21 +0200

    Default image placed for all FAQ questions

--- templates/sections/faq.php
+++ templates/sections/faq.php
@@ -1,10 +1,13 @@
 <?php include(__DIR__ . '/../../paletteSystem.php'); ?>
 
 <?php
+
+// ACF Image Fallback
+$fallbackImageUrl = get_field("faq_fallback_image", 1140);
+
 $args = array(
     'post_type' => 'questions'
 );
-
 $loop = new WP_Query($args);
 
 ?>
@@ -12,7 +15,9 @@ $loop = new WP_Query($args);
     <?php while ($loop->have_posts()) : $loop->the_post(); ?>
         <div class="flex flex-col items-center justify-start rounded-2xl bg-[<?php echo $SnowWhite; ?>] shadow-lg shadow-[<?php echo $BabyBlueShadow; ?>] w-full md:max-w-[300px] min-h-[420px]">
             <!-- Default Thumbnail -->
-            <img class="rounded-t-md" src=" http://staxtalks-local.local/wp-content/uploads/2022/10/fallback-thumbnail.png" alt="">
+            <?php if ($fallbackImageUrl) { ?>
+                <img class="rounded-t-md" src="<?php echo $fallbackImageUrl; ?>" alt="">
+            <?php } ?>
             <!-- Post Content -->
             <div class="w-full p-4">
                 <h3 class="text-large"><?php the_title(); ?></h3>
